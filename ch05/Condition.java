d1b6b4d2b1c655480308c3a19ce2211979478653
marko <markolsen26@gmail.com>
2018-07-26 16:37:26 -0500

    Comparison Exercise

---
+++ ch05/Condition.java
@@ -0,0 +1,14 @@
+public class Condition
+
+{
+    public static void main(String[] args)
+    {
+        int num1 = 1357;
+        int num2 = 2468;
+        String string = "result";
+        String result = (num1 % 2 !=0) ? "Odd" : "Even";
+        System.out.println(result = (num2 + "is" + result));
+
+    }
+
+}
